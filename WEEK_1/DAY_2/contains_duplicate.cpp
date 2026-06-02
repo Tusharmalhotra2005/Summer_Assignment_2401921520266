4d7e5904b9e8a55201a3e4841c7122091c586115
Tusharmalhotra2005 <tushar.malhotra2005@gmail.com>
2026-06-02 13:46:20 +0530

    Contains duplicate

---
+++ WEEK_1/DAY_2/contains_duplicate.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map <int, int> mapp;
+
+        for(int num :nums){
+            mapp[num]++;
+        }
+
+        for(auto &p:mapp){
+            if(p.second >= 2 ){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
