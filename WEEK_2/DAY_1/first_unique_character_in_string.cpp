c484a9689f862b742837f905d75fe2454e7edfb4
Tusharmalhotra2005 <tushar.malhotra2005@gmail.com>
2026-06-08 18:23:47 +0530

    First unique element in string

---
+++ WEEK_2/DAY_1/first_unique_character_in_string.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int frequency[26]={0};
+
+        for(char ch:s){
+            frequency[ch-'a']++;
+        }
+
+        for(int i=0 ; i<s.size() ; i++){
+            if(frequency[s[i]-'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
