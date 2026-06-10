62b6750f41b567cc379c310a52063829aa15bb69
Tusharmalhotra2005 <tushar.malhotra2005@gmail.com>
2026-06-10 13:55:30 +0530

    Is sequence

---
+++ WEEK_2/DAY_3/is_sequence.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=0 , j=0;
+        int m=s.size() , n=t.size();
+
+        while(i<m && j<n){
+            if(s[i] == t[j]){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        if(i==m) return true;
+        else return false;
+
+    }
+};
